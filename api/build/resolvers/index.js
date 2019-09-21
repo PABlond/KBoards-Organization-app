aca7b013574e444b6de520c11c87cf9a5f572577
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-09-21 21:19:31 +0200

    deleting build

--- api/build/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_resolvers_1 = __importDefault(require("./auth.resolvers"));
-const boards_resolvers_1 = __importDefault(require("./boards.resolvers"));
-exports.default = Object.assign(Object.assign({}, auth_resolvers_1.default), boards_resolvers_1.default);
-//# sourceMappingURL=index.js.map
